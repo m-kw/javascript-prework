f048cd9b383910213f7ef9c2759a28de9b0c97cb
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-09-04 21:08:42 +0200

    Add playerMove script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Computer move
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -13,3 +15,21 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// Player move
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
